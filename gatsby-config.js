e7fc3569f3c6ef5a828a693cab9c6e9e0daf9883
Choji Kaseda <kasedac@users.noreply.github.com>
2022-02-01 23:17:36 +0900

    siteのmetadataを修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Blog Sandbox`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `kasedac`,
+      summary: `SSGでのBlog構築を模索中。`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `GatsbyJSによるBlogの実験場.`,
+    siteUrl: `https://kasedac.github.io/blog-sandbox/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `kasedac`,
     },
   },
   plugins: [
